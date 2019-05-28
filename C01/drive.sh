e56726bd3786992c88e3f9e29290a4c01c6687fc
hexu <hexu_1985@sina.com>
2019-05-28 14:12:11 +0800

    add README, C01, C02

---
+++ C01/drive.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+
+./P1_1 <<EOF
+3
+EOF
+
+echo ""
+
+./P1_2
+
+echo ""
+
+./P1_3
+
+echo ""
+
+./P1_4 <<EOF
+298
+28
+EOF
+
+echo ""
+
+./P1_5 <<EOF
+4
+EOF
+
+./P1_5 <<EOF
+-13
+EOF
+
+echo ""
+
+./P1_6
+
+echo ""
+
+./P1_7 <<EOF
+abcTefg
+EOF
+
+echo ""
+
+./P1_8
+
+echo ""
+
+./P1_10 <<EOF
+10
+EOF
+
+echo ""
+
+./P1_11 <<EOF
+1
+EOF
+
+echo ""
+
+./P1_11 <<EOF
+2
+EOF
+
+echo ""
+
+./P1_11 <<EOF
+3
+EOF
+
+echo ""
+
+./P1_11 <<EOF
+4
+EOF
+
+echo ""
+
+./P1_12 <<EOF
+1.57
+EOF
+
+echo ""
+
+./P1_13
+
+echo ""
+
+./P1_14 <<EOF
+25
+EOF
+
+echo ""
+
+./P1_15
+
+echo ""
+
+./P1_16
+
+echo ""
+
+./P1_17
+
+echo ""
+
+./P1_18
+
+echo ""
+
+./P1_19
+
+echo ""
+
+./P1_20 <<EOF
+6
+1
+2
+3
+4
+5
+6
+3
+11
+12
+13
+21
+22
+23
+31
+32
+33
+EOF
+
+echo ""
+
+./P1_21
+
+echo ""
+
